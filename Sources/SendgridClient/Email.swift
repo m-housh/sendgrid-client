db91c2de7a7560c94382d0e9a708a990d8b86c03
Michael Housh <mhoush@houshhomeenergy.com>
2018-12-09 13:52:19 -0500

    Added initializer to Email.

--- Sources/SendgridClient/Email.swift
+++ Sources/SendgridClient/Email.swift
@@ -10,5 +10,10 @@ import Vapor
 public struct Email: Content {
     public let email: String
     public let name: String?
+    
+    public init(email: String, name: String?) {
+        self.email = email
+        self.name = name
+    }
 }
 
