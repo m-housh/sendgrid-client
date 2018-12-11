cc257c0888ca8edc1a007ee838e1fde1f09aa6e3
Michael Housh <mhoush@houshhomeenergy.com>
2018-12-10 23:28:12 -0500

    Added templates

--- Sources/SendgridClient/Template/TemplateType.swift
+++ Sources/SendgridClient/Template/TemplateType.swift
@@ -10,6 +10,8 @@ import Foundation
 
 public enum TemplateType {
     case preQuote
+    case preSalesVisit
+    case postService
 }
 
 extension TemplateType {
@@ -18,6 +20,10 @@ extension TemplateType {
         switch self {
         case .preQuote:
             return "d-399fe5e4af16408f9ba34e213b41fa57"
+        case .preSalesVisit:
+            return "d-c84cf663fdb844bc8947b87e38122946"
+        case .postService:
+            return "d-57d634c568134942bc251681b29d6dc7"
         }
     }
     
--- Tests/SendgridClientTests/SendgridClientTests.swift
+++ Tests/SendgridClientTests/SendgridClientTests.swift
@@ -14,9 +14,46 @@ enum SendgridTestError: Error {
     case emailNotFound
 }
 
+extension TemplateType {
+   static func all() -> [TemplateType] {
+        return [TemplateType.preQuote, TemplateType.preSalesVisit, TemplateType.postService]
+    }
+}
+
 final class SendgridTests: XCTestCase, VaporTestCase {
     
     var app: Application!
+    
+    override func setUp() {
+        perform {
+            self.app = try makeApplication()
+        }
+    }
+    
+    func testClient() {
+        perform {
+            
+            let sg_client = try app.make(SendgridClient.self)
+            let email = try self.email()
+            let senderName = email.name ?? "Test Sender Name"
+            
+            let personalization = Personalization(to: [email], senderName: senderName)
+            
+            for template in TemplateType.all() {
+                let req = template.request(personalizations: [personalization], from: email, reply_to: email)
+                
+                _ = try sg_client.send(req, on: app).wait()
+            }
+            
+        }
+    }
+}
+
+extension SendgridTests {
+    
+    func client() throws -> SendgridClient {
+        return try app.make(SendgridClient.self)
+    }
     
     func email() throws -> Email {
         guard let email = Environment.get("SG_EMAIL") else {
@@ -28,28 +65,4 @@ final class SendgridTests: XCTestCase, VaporTestCase {
         return Email(email: email, name: name)
     }
     
-    override func setUp() {
-        perform {
-            self.app = try makeApplication()
-        }
-    }
-    
-    func testClient() {
-        perform {
-            let sg_client = try app.make(SendgridClient.self)
-            let email = try self.email()
-            let senderName = email.name ?? "Test Sender Name"
-            
-            let personalization = Personalization(to: [email], senderName: senderName)
-            
-            let req = TemplateType.preQuote.request(
-                personalizations: [personalization],
-                from: email,
-                reply_to: email
-            )
-            
-            _ = try sg_client.send(req, on: app).wait()
-            
-        }
-    }
 }
